f2e382ba4813b1631add192942c182f19f0a9480
Sheldrick <mrmayrant@yahoo.com>
2019-06-09 18:57:43 -0400

    added user information from the API to the user component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { Fragment, Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/Users/Users";
+import User from "./components/Users/User";
 import Search from "./components/Users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/Pages/About";
 import axios from "axios";
 import "./App.css";
 
-class App extends React.Component {
+class App extends Component {
 	state = {
 		users: [],
+		user: {},
 		loading: false,
 		alert: null
 	};
@@ -21,6 +25,16 @@ class App extends React.Component {
 				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}}`
 		);
 		this.setState({ users: res.data.items, loading: false });
+	};
+
+	// get single user
+	getUser = async username => {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/users/${username}?client_id=${process.env
+				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}}`
+		);
+		this.setState({ user: res.data, loading: false });
 	};
 
 	// clear the users that are currently in state
@@ -36,21 +50,41 @@ class App extends React.Component {
 	};
 
 	render () {
-		const { loading, users } = this.state;
+		const { loading, users, user } = this.state;
 		return (
-			<div className="App">
-				<Navbar title=" Github Finder" />
-				<div className="container">
-					<Alert alert={this.state.alert} />
-					<Search
-						searchUsers={this.searchUsers}
-						clearUsers={this.clearUsers}
-						showClear={users.length > 0 ? true : false}
-						setAlert={this.setAlert}
-					/>
-					<Users loading={loading} users={users} />
+			<Router>
+				<div className="App">
+					<Navbar title=" Github Finder" />
+					<div className="container">
+						<Alert alert={this.state.alert} />
+						<Switch>
+							<Route
+								exact
+								path="/"
+								render={props => (
+									<Fragment>
+										<Search
+											searchUsers={this.searchUsers}
+											clearUsers={this.clearUsers}
+											showClear={users.length > 0 ? true : false}
+											setAlert={this.setAlert}
+										/>
+										<Users loading={loading} users={users} />
+									</Fragment>
+								)}
+							/>
+							<Route exact path="/about" component={About} />
+							<Route
+								exact
+								path="/user/:login"
+								render={props => (
+									<User {...props} getUser={this.getUser} user={user} loading={loading} />
+								)}
+							/>
+						</Switch>
+					</div>
 				</div>
-			</div>
+			</Router>
 		);
 	}
 }
---
+++ src/components/Users/User.js
@@ -0,0 +1,106 @@
+import React, { Fragment, Component } from "react";
+import Spinner from "../layout/Spinnger";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+class User extends Component {
+	componentDidMount () {
+		this.props.getUser(this.props.match.params.login);
+	}
+
+	static propTypes = {
+		loading: PropTypes.bool,
+		user: PropTypes.object.isRequired,
+		getuser: PropTypes.func.isRequired
+	};
+	render () {
+		const {
+			name,
+			company,
+			avatar_url,
+			location,
+			bio,
+			blog,
+			login,
+			html_url,
+			followers,
+			following,
+			public_repos,
+			public_gists,
+			hireable
+		} = this.props.user;
+
+		const { loading } = this.props;
+
+		if (loading) {
+			return <Spinner />;
+		}
+
+		return (
+			<Fragment>
+				<Link to="/" className="btn btn-light">
+					Back To Search
+				</Link>
+				Hireable: {""}
+				{/* Display checkmark icon if user is hireable, display x icon if not */}
+				{hireable ? (
+					<i className="fas fa-check text-success" />
+				) : (
+					<i className="fas fa-times-circle text-danger" />
+				)}
+				<div className="card grid-2">
+					<div className="all-center">
+						<img src={avatar_url} className="round-img" alt="user" style={{ width: "150px" }} />
+						<h1>{name}</h1>
+						<p>Location: {location}</p>
+					</div>
+					<div>
+						{bio && (
+							<Fragment>
+								<h3>Bio</h3>
+								<p>{bio}</p>
+							</Fragment>
+						)}
+						<a href={html_url} className="btn btn-dark my-1">
+							Visit Github Profile
+						</a>
+						<ul>
+							<li>
+								{login && (
+									<Fragment>
+										<strong>Username: </strong>
+										{login}
+									</Fragment>
+								)}
+							</li>
+							<li>
+								{company && (
+									<Fragment>
+										<strong>Company: </strong>
+										{company}
+									</Fragment>
+								)}
+							</li>
+							<li>
+								{blog && (
+									<Fragment>
+										<strong>Website: </strong>
+										<a href={blog}>{blog}</a>
+									</Fragment>
+								)}
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className="text-center card">
+					<div className="badge badge-light">Followers: {followers}</div>
+					<div className="badge badge-success">Following: {following}</div>
+					<div className="badge badge-danger">Public Repos: {public_repos}</div>
+					<div className="badge badge-dark">Public Gists: {public_gists}</div>
+				</div>
+			</Fragment>
+		);
+	}
+}
+
+export default User;
